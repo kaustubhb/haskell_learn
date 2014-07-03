681f5bdab863ab6de01c15b96e668f316109c74c
Kaustubh <kaustubh.cool@gmail.com>
2014-07-03 18:01:02 +0530

    Started recursion

--- recursion.hs
+++ recursion.hs
@@ -7,3 +7,16 @@ replicate' :: (Ord a, Integral a) => a -> b -> [b]
 replicate' n x
     | n <= 0 = []
     | otherwise = x : replicate' (n-1) x
+
+take' :: Int-> [b] -> [b]
+take' n _ 
+    | n <= 0 = []
+take' _ [] = []
+take' n (x:xs) = x : take' (n-1) xs
+
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x:xs) = (reverse' xs) ++ [x]
+
+repeat' :: a -> [a]
+repeat' x = x : repeat' x
