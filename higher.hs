0064d761c19fd4515ecd33b84e13efa02980a498
Kaustubh <kaustubh.cool@gmail.com>
2014-07-06 12:21:05 +0530

    Higher order functions

--- higher.hs
+++ higher.hs
@@ -7,6 +7,35 @@ zipWith' _ _ [] = []
 zipWith' func (x:xs) (y:ys) = 
     (func x y):(zipWith' func xs ys)
 
-flip' :: (a -> b -> c) -> (b -> a -> c)
-flip' f = g
-    where g y x = f x y
+flip' :: (a -> b -> c) -> b -> a -> c
+flip' f x y = f y x
+
+map' :: (a -> b) -> [a] -> [b]
+map' _ [] = []
+map' f (x:xs) = (f x) : (map' f xs)
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' _ [] = []
+filter' f (x:xs)
+    | f x == True = x:(filter' f xs)
+    | otherwise = filter' f xs
+
+-- largest number under 100,000 divisible by 3289
+largestDivisible :: (Integral a) => a
+largestDivisible = head (filter f [10000000,9999999..])
+    where f x = mod x 3289 == 0
+
+-- sum of all odd sqaures smaller than 10,000
+sumSquares :: (Integral a) => a
+sumSquares = sum (takeWhile (< 10000) (map (^2) [1,3..]))
+
+-- check numbers below 100, whose collatz chain is larger than 15 
+chain :: (Integral a) => a -> [a]
+chain 1 = [1]
+chain x 
+    | odd x = x : (chain (3*x + 1))
+    | otherwise = x : (chain (x `div` 2))
+
+collatz :: (Integral a) -> a -> Int
+collatz x = length (filter isLong (map chain [1..x]))
+          where isLong xs = (length xs) > 15
